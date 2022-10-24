8d46fd033779175b43cfc3066f1067db33f3f932
Gökalp Avcu <avcu219@gmail.com>
2022-10-24 17:14:53 +0300

    add changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 
 function App() {
 
- 
+const [click, setClick] = useState(true);
 
+
+  async function getAdvice() {
+    try {
+      const response = await axios.get("https://api.adviceslip.com/advice"); // await: bu fonk. bir yanıt verene kadar bekle, yanıt geldiği zaman bu değişkeni oluştur
+      console.log(response); // eğer yanıt geldiyse, bu yanıtı konsola yazdır
+    } catch (error) {
+      console.error(error); 
+    }
+  }
+
+  useEffect(() => {
+     getAdvice();
+  }, [click]); // dependecy'nin içi boşsa sadece ilk renderda çalışır, içine bir şey yazarsak o değiştiğinde çalışır.
+
+const clickHandler = () => { 
+    setClick(!click); // click'i tersine çevir
+  }
 
   return (
     <div className="App">
-     <h1 className='text-3xl font-bold underline'>HELLO WORLD</h1>
+      <h1 className="text-3xl font-bold underline">
+        <button onClick={() => clickHandler()}>PUSH</button>
+      </h1>
     </div>
   );
 }
+
 
 export default App;
