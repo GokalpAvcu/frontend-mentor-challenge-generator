58e10def4c3fe64a735267ffa8b96d589a79bb4e
Gökalp Avcu <avcu219@gmail.com>
2022-10-26 14:51:40 +0300

    add changes

--- src/Advice.jsx
+++ src/Advice.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 
-const Advice = ({advice, setClick}) => {
-console.log("comp", advice)
+const Advice = ({ advice, setClick}) => {
+
+  console.log(advice);
 
   const clickHandler = () => {
     setClick((prev) => !prev); // click'i tersine çevir
   };
 
-  return <button onClick={() => clickHandler()}>PUSH</button>;
+  return (
+    <div>
+      <h1>{advice.slip.id}</h1>
+      <h1>{advice.slip.advice}</h1>
+      <button onClick={() => clickHandler()}>PUSH</button>;
+    </div>
+  );
 };
 
 export default Advice;
